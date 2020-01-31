7710454a2b3c7af3117d92f4acf8ef1326a144e8
fabio araujo <araujosilvafabio@hotmail.com>
2020-01-31 16:43:23 -0200

    Atualizacao

--- SPRINT1-2DEV-OPTUS-FABIO.sql
+++ SPRINT1-2DEV-OPTUS-FABIO.sql
@@ -40,6 +40,8 @@ ALTER TABLE Artistas ADD Idade INT;
 ALTER TABLE Artistas DROP COLUMN Idade;
 ALTER TABLE Usuarios DROP COLUMN Idade;
 ALTER TABLE Albuns ALTER COLUMN Visualizacao BIGINT;
+ALTER TABLE Artistas ADD Id_Estilo INT FOREIGN KEY REFERENCES Estilos (Id_Estilo);
+
 
 -- DML
 
@@ -78,8 +80,50 @@ UPDATE Artistas SET Nome_Artista = 'Zeca Pagodinho' WHERE Id_Artista = 2;
 UPDATE Artistas SET Nome_Artista = 'Ariana Grande' WHERE Id_Artista = 5;
 DELETE FROM Albuns WHERE Id_Album = 1;
 
+UPDATE Artistas SET Id_Estilo = 1 WHERE Id_Artista = 1;
+UPDATE Artistas SET Id_Estilo = 2 WHERE Id_Artista = 2;
+UPDATE Artistas SET Id_Estilo = 3 WHERE Id_Artista = 3;
+UPDATE Artistas SET Id_Estilo = 1 WHERE Id_Artista = 4;
+UPDATE Artistas SET Id_Estilo = 4 WHERE Id_Artista = 5;
+
 UPDATE Albuns SET Visualizacao = 1 WHERE Id_Album = 2;
+UPDATE Albuns SET Data_Lancamento = '28/02/2020' WHERE Id_Album = 3;
+UPDATE Albuns SET Data_Lancamento = '10/07/2010' WHERE Id_Album = 4;
+UPDATE Albuns SET Data_Lancamento = '14/06/2003' WHERE Id_Album = 5;
 
 UPDATE Usuarios SET Id_TipoUsuario = 1 WHERE Id_Usuario = 3;
 
+UPDATE Albuns SET Quantidade_Minutos = 20 WHERE Id_Album = 2;
+
 DELETE FROM Albuns WHERE Id_Album = 2;
+
+
+-- DQL 
+
+SELECT Nome_Artista FROM Artistas;
+
+SELECT Data_Lancamento FROM Albuns;
+
+SELECT Nome_Album FROM Albuns WHERE Id_Album <> 2;
+
+SELECT Nome_Album, Visualizacao FROM Albuns WHERE (Data_Lancamento IS NULL) or (Quantidade_Minutos IS NULL);
+
+SELECT Id_Album, Nome_Album FROM Albuns WHERE Nome_Album LIKE '%ouro%';
+
+SELECT Nome_Album FROM Albuns ORDER BY Nome_Album;
+
+SELECT Id_Album, Nome_Album, Quantidade_Minutos FROM Albuns ORDER BY Quantidade_Minutos DESC; 
+
+SELECT Id_Album, Nome_Album, Quantidade_Minutos FROM Albuns ORDER BY Quantidade_Minutos ASC; 
+
+SELECT COUNT(Id_Album) FROM Albuns;
+
+------------------------------------------ AVALIAÇÃO DQL -------------------------------------------------
+
+SELECT Id_Album, Nome_Album FROM Albuns WHERE Id_Artista = 2;
+
+SELECT Id_Album, Nome_Album, Data_Lancamento FROM Albuns WHERE Data_Lancamento = '29/01/2020'; 
+
+SELECT Id_Artista, Nome_Artista, Id_Estilo FROM Artistas WHERE Id_Estilo = 1;
+
+SELECT Id_Album, Nome_Album, Id_Artista, Data_Lancamento FROM Albuns ORDER BY Data_Lancamento DESC; 
